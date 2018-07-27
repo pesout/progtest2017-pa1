237d7d52f0cc4f4365f2f127d1c77771e14bcfb3
Štěpán Pešout <stepan@pesout.eu>
2018-07-27 13:03:56 +0200

    Formated

--- progtest0.c
+++ progtest0.c
@@ -1,40 +1,31 @@
 #include "stdio.h"
 
-int main ( int argc, char * argv [] )
-{
+int main(int argc, char * argv[]) {
   float a;
 
   printf("ml' nob:\n");
-  scanf("%f", &a);
+  scanf("%f", & a);
 
-  if (a != 1 && a != 2 && a != 3 && a != 4 && a != 5)
-  {
+  if (a != 1 && a != 2 && a != 3 && a != 4 && a != 5) {
     printf("luj");
-  }
-  else
-  {
-    if (a == 1)
-    {
+  } else {
+    if (a == 1) {
       printf("Qapla'\nnoH QapmeH wo' Qaw'lu'chugh yay chavbe'lu' 'ej wo' choqmeH may' DoHlu'chugh lujbe'lu'.\n");
     }
 
-    if (a == 2)
-    {
+    if (a == 2) {
       printf("Qapla'\nQu' buSHa'chugh SuvwI', batlhHa' vangchugh, qoj matlhHa'chugh, pagh ghaH SuvwI''e'.\n");
     }
 
-    if (a == 3)
-    {
+    if (a == 3) {
       printf("Qapla'\nqaStaHvIS wa' ram loS SaD Hugh SIjlaH qetbogh loD.\n");
     }
 
-    if (a == 4)
-    {
+    if (a == 4) {
       printf("Qapla'\nHa'DIbaH DaSop 'e' DaHechbe'chugh yIHoHQo'.\n");
     }
 
-    if (a == 5)
-    {
+    if (a == 5) {
       printf("Qapla'\nleghlaHchu'be'chugh mIn lo'laHbe' taj jej.\n");
     }
   }
